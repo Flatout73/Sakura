7af4609bdfa6c2c495b71ad4e6eac0c80774fdac
Леонид Лядвейкин <lalyadveykin@edu.hse.ru>
2016-05-15 16:20:51 +0300

    bugFixes

--- sakura/sakura/Vertex.cs
+++ sakura/sakura/Vertex.cs
@@ -136,8 +136,11 @@ namespace sakura
             //   spriteBatch.Draw(textureFlower, position + new Vector2(textureFlower.Width/2, textureFlower.Height/2), 
             //     null, Color.White, (float)(Math.PI/ 2.0f) * (float)touches, new Vector2(textureFlower.Width / 2, textureFlower.Height / 2), 1 , SpriteEffects.None, 0f);
 
-			spriteBatch.Draw(textureFlower, position + new Vector2(textureFlower.Width / 2.0f + ((float)flowerWidth * kx / 2.0f - (float)flowerWidth/2.0f)/2.0f, textureFlower.Height / 2.0f + ((float)flowerHeight * kx / 2.0f - flowerWidth/2.0f)/2.0f),
-                null, Color.White, (float)(Math.PI / 2.0f) * (float)touches, new Vector2(textureFlower.Width / 2.0f, textureFlower.Height / 2.0f), (float)flowerWidth / (float)textureFlower.Width * kx, SpriteEffects.None, 0f);
+            //		spriteBatch.Draw(textureFlower, position + new Vector2(textureFlower.Width / 2.0f + ((float)flowerWidth * kx / 2.0f - (float)flowerWidth/2.0f), textureFlower.Height / 2.0f + ((float)flowerHeight * kx / 2.0f - flowerWidth/2.0f)),
+            //           null, Color.White, (float)(Math.PI / 2.0f) * (float)touches, new Vector2(textureFlower.Width / 2.0f, textureFlower.Height / 2.0f), (float)flowerWidth / (float)textureFlower.Width * kx, SpriteEffects.None, 0f);
+
+            Rectangle flrect = new Rectangle((int)(position.X + flowerWidth * kx / 2f), (int)(position.Y + flowerHeight * kx / 2f), (int)((float)flowerWidth * kx), (int)((float)flowerHeight * kx));
+            spriteBatch.Draw(textureFlower, flrect, null, Color.White, (float)(Math.PI / 2f) * (float)touches, new Vector2(textureFlower.Width / 2, textureFlower.Height / 2), SpriteEffects.None, 0f);
 
             Rectangle rect = new Rectangle((int)(position.X + (float)flowerWidth * kx / 2.0f - (float)leafWidth * kx / 2.0f), (int)(position.Y - (float)leafHeight * kx), (int)((float)(leafWidth * kx)), (int)((float)leafHeight * kx));
              if (edgeUp)
